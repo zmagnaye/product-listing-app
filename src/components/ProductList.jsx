f64f5436f928c5eecf738cea7582d43e6b64963e
Zari <zjcmagnaye@gmail.com>
2025-06-11 22:06:55 -0700

    Added ProductList component that maps out products to ProductItem

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,9 +1,13 @@
-function ProductList() {
+import ProductItem from './ProductItem'
 
+function ProductList({ products}) {
     return (
-        <>
-        
-        </>
+        <div className="products-container">
+            <h1>Product List Component</h1>
+            {products.map(product => (
+                <ProductItem key={product.id} product={product} />
+            ))}
+        </div>>
     )
 }
 
